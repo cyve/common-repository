10121ba81a7061499f5ac054e33e5f560c909b33
Cyril Vmd <cyril@cyrilwebdesign.com>
2018-11-26 18:53:57 +0100

    Add EntityRepositoryInterface

---
+++ src/EntityRepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Cyve\EntityRepository;
+
+interface EntityRepositoryInterface
+{
+    public function find($id);
+    public function findAll();
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null);
+    public function findOneBy(array $criteria);
+    public function getClassName();
+    public function save($entity, bool $flush = true);
+    public function remove($entity, bool $flush = true);
+    public function flush();
+    public function count(array $criteria = []);
+    public function searchBy(array $criteria, array $orderBy = null, $limit = null, $offset = null);
+}
