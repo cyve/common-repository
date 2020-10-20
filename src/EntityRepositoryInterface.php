165a351fa7e12ebcc4a16c0ba6f1a0729737a7e8
Cyril Vermande <cyril@cyrilwebdesign.com>
2020-10-20 21:20:44 +0200

    Fix iterateBy() and searchBy() signature

--- src/EntityRepositoryInterface.php
+++ src/EntityRepositoryInterface.php
@@ -13,7 +13,7 @@ interface EntityRepositoryInterface extends ObjectRepository
     public function refresh($entity): void;
     public function flush(): void;
     public function iterate(int $hydrationMode = 1): \Iterator;
-    public function iterateBy(array $criteria, array $orderBy = null, int $limit = null, int $offset = null, int $hydrationMode = 1): \Iterator;
-    public function searchBy(array $criteria, array $orderBy = null, int $limit = null, int $offset = null): array;
+    public function iterateBy(array $criteria, array $orderBy =  [], int $limit = null, int $offset = null, int $hydrationMode = 1): \Iterator;
+    public function searchBy(array $criteria, array $orderBy = [], int $limit = null, int $offset = null): array;
     public function countSearchBy(array $criteria): int;
 }
